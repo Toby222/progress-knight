ea6aa72f89643f57a080901f73783cd02e073446
LexieN <34977021+SylanGH@users.noreply.github.com>
2021-01-18 10:37:35 -0600

    arrow key navigation

--- js/input.js
+++ js/input.js
@@ -1,25 +1,33 @@
-function changeTab(direction){
-    var tabs = Array.prototype.slice.call(document.getElementsByClassName("tab"))
-    var tabButtons = Array.prototype.slice.call(document.getElementsByClassName("tabButton"))
+//originally by Pseiko
 
-    var currentTab = 0
-    for (i in tabs) {
+function changeTab(direction)
+{
+    var tabs = Array.prototype.slice.call(document.getElementsByClassName("tab"));
+    var tabButtons = Array.prototype.slice.call(document.getElementsByClassName("tabButton"));
+
+    var currentTab = 0;
+    for (i in tabs) 
+    {
         if (!tabs[i].style.display.includes("none"))
-             currentTab = i
+             currentTab = i*1;
     }
-    var targetTab = currentTab+direction
-    targetTab = Math.max(0,targetTab)
-    if( targetTab > (tabs.length-1)) targetTab = 0
-    while(tabButtons[targetTab].style.display.includes("none")){
-        targetTab = targetTab+direction
-        targetTab = Math.max(0,targetTab) 
-        if( targetTab > (tabs.length-1)) targetTab = 0
+    
+    var targetTab = currentTab+direction;
+    
+    if( targetTab > (tabs.length - 1)) 
+    {
+        targetTab = 0;
     }
-    setTab(document.getElementById(tabs[targetTab].id+"TabButton"), tabs[targetTab].id)
+    
+    if(targetTab < 0)
+    {
+        targetTab = (tabs.length - 1);
+    }
+    
+    setTab(tabButtons[targetTab], tabs[targetTab].id);
 } 
 
 document.onkeydown =  function(e){
-    console.log(e.key)
     if(e.key==" ") setPause() 
     if(e.key=="ArrowRight") changeTab(1) 
     if(e.key=="ArrowLeft") changeTab(-1) 
