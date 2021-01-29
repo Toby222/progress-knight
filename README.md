195370568e5f727e2b39afee4b602642b5e66108
Tobias Berger <14962962+Toby222@users.noreply.github.com>
2021-01-29 09:35:32 +0000

    Lint

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # mod-knight
+
 a mod for a game created by ihtasham42. The original is here: https://github.com/ihtasham42/progress-knight Link to the game: https://ihtasham42.github.io/progress-knight/
 
 The mod can be played here:
--- js/input.js
+++ js/input.js
@@ -1,35 +1,31 @@
 //originally by Pseiko
 
-function changeTab(direction)
-{
-    var tabs = Array.prototype.slice.call(document.getElementsByClassName("tab"));
-    var tabButtons = Array.prototype.slice.call(document.getElementsByClassName("tabButton"));
+function changeTab(direction) {
+  const tabs = Array.prototype.slice.call(document.getElementsByClassName("tab"));
+  const tabButtons = Array.prototype.slice.call(document.getElementsByClassName("tabButton"));
 
-    var currentTab = 0;
-    for (i in tabs) 
-    {
-        if (!tabs[i].style.display.includes("none"))
-             currentTab = i*1;
-    }
-    
-    var targetTab = (currentTab+direction) % tabs.length;
-    
-    if(targetTab < 0)
-    {
-        targetTab = (tabs.length - 1);
-    }
-    
-    //check for tabs not unlocked
-    while(tabButtons[targetTab].style.display.includes("none") || tabButtons[targetTab].className.includes("hidden") || tabButtons[targetTab].className.includes("hiddenTask")){
-        targetTab = (targetTab+direction) % tabs.length;
-        targetTab = Math.max(0,targetTab);
-    }
-    
-    setTab(tabButtons[targetTab], tabs[targetTab].id);
-} 
+  let currentTab = 0;
+  for (const i in tabs) {
+    if (!tabs[i].style.display.includes("none")) currentTab = i * 1;
+  }
 
-document.onkeydown =  function(e){
-    if(e.key==" ") setPause() 
-    if(e.key=="ArrowRight") changeTab(1) 
-    if(e.key=="ArrowLeft") changeTab(-1) 
+  let targetTab = (currentTab + direction) % tabs.length;
+
+  if (targetTab < 0) {
+    targetTab = tabs.length - 1;
+  }
+
+  //check for tabs not unlocked
+  while (tabButtons[targetTab].style.display.includes("none") || tabButtons[targetTab].className.includes("hidden") || tabButtons[targetTab].className.includes("hiddenTask")) {
+    targetTab = (targetTab + direction) % tabs.length;
+    targetTab = Math.max(0, targetTab);
+  }
+
+  setTab(tabButtons[targetTab], tabs[targetTab].id);
 }
+
+document.onkeydown = function (e) {
+  if (e.key === " ") setPause();
+  if (e.key === "ArrowRight") changeTab(1);
+  if (e.key === "ArrowLeft") changeTab(-1);
+};
